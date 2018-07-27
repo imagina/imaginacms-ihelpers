c78ec505b2b339d6d7c57cea4f6968f9ca838126
Carlos Devia <carlosdevia@imagincolombia.com>
2018-07-27 18:10:35 -0500

    +Add: Functions istr_slug to allow ("." dots) in slugs

--- helpers.php
+++ helpers.php
@@ -1,4 +1,5 @@
 <?php
+use Illuminate\Support\Str;
 
 if(! function_exists('canonical_url')) {
 
@@ -8,5 +9,21 @@ if(! function_exists('canonical_url')) {
 
 }
 
+if(! function_exists('istr_slug')) {
+    function istr_slug($title, $separator = '-', $language = 'en',$allowedchars=array())
+    {
+        $title = Str::ascii($title, $language);
+        // Convert all dashes/underscores into separator
+        $flip = $separator == '-' ? '_' : '-';
+        $title = preg_replace('![' . preg_quote($flip) . ']+!u', $separator, $title);
+        // Replace @ with the word 'at'
+        $title = str_replace('@', $separator . 'at' . $separator, $title);
+        // Remove all characters that are not the separator, letters, numbers, or whitespace.
+        $title = preg_replace('![^' . preg_quote($separator) . '^' . preg_quote(".") . '\pL\pN\s]+!u', '', mb_strtolower($title));
+        // Replace all separator characters and whitespace by a single separator
+        $title = preg_replace('![' . preg_quote($separator) . '\s]+!u', $separator, $title);
+        return trim($title, $separator);
+    }
+}
 
 ?>
