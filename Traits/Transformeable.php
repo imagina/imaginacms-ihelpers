4d51e79340d5ed45129b6120a87fd7ab365d12fc
JCEC007 <jcec007@gmail.com>
2020-10-17 15:23:02 -0500

    +fix: Transformeable Trait

--- Traits/Transformeable.php
+++ Traits/Transformeable.php
@@ -3,19 +3,20 @@ namespace Modules\Ihelpers\Traits;
 
 trait Transformeable
 {
-
+  
   /**
    * get custom includes into a transformer
    * @param $data
    */
   public function customIncludes(&$data){
-
-    $classNamespace = get_class($this);
-    $classNamespaceExploded = explode('\\',$classNamespace);
-    \Log::info($classNamespaceExploded);
-
-    $customProductIncludes = config('asgard.'.strtolower($classNamespaceExploded[1]).'.config.includes.'.$classNamespaceExploded[3]);
-
+    
+    $classNamespace = get_class($this->resource);
+    $classNamespaceExploded = explode('\\',strtolower($classNamespace));
+    
+    
+    $customProductIncludes = config('asgard.'.strtolower($classNamespaceExploded[1]).'.config.includes.'.$classNamespaceExploded[3]) ?? [];
+    
+    
     foreach ($customProductIncludes as $include=>$customProductInclude){
       if($customProductInclude['multiple']){
         $data[$include] = $customProductInclude['path']::collection($this->whenLoaded($include));
