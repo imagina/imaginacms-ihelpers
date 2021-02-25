4dd0e99e37b9dfbb63c2dcf1c5fa9ebc314230e9
Slim Arias Sanchez <slimarias1987@gmail.com>
2021-02-25 12:02:55 -0500

    +FIX: Transformeable include did not get field from config

--- Traits/Transformeable.php
+++ Traits/Transformeable.php
@@ -3,25 +3,25 @@ namespace Modules\Ihelpers\Traits;
 
 trait Transformeable
 {
-  
+
   /**
    * get custom includes into a transformer
    * @param $data
    */
   public function customIncludes(&$data){
-    
+
     $classNamespace = get_class($this->resource);
     $classNamespaceExploded = explode('\\',strtolower($classNamespace));
-    
-    
+
+
     $customProductIncludes = config('asgard.'.strtolower($classNamespaceExploded[1]).'.config.includes.'.$classNamespaceExploded[3]) ?? [];
-    
-    
+
+
     foreach ($customProductIncludes as $include=>$customProductInclude){
       if($customProductInclude['multiple']){
-        $data[$include] = $customProductInclude['path']::collection($this->whenLoaded($include));
+        $data[$include] = $customProductInclude['path']::collection($this->$include()->get() ?? []);
       }else{
-        $data[$include] = new $customProductInclude['path']($this->whenLoaded($include));
+        $data[$include] = new $customProductInclude['path']($this->$include()->first() ?? null);
       }
     }
   }
