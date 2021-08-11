d31aa0ad12103f8edfc2feaff27c8f4e9adfb51d
Juan Escalante <jcec007@gmail.com>
2021-08-11 18:48:50 -0500

    +Add: UniqueRule to validate unique columns

---
+++ Rules/UniqueRule.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace Modules\Ihelpers\Rules;
+
+use Illuminate\Contracts\Validation\Rule;
+
+class UniqueRule implements Rule
+{
+    /**
+     * Create a new rule instance.
+     *
+     * @return void
+     */
+    public $table;
+    public $id;
+    public $columnId;
+    public $message;
+    
+    public function __construct($table, $id = null, $columnId = "", $message = "")
+    {
+      $this->table = $table;
+      $this->id = $id;
+      $this->columnId = $columnId;
+      $this->message = !empty($message) ? $message : 'There are another register with the same email.';
+    }
+
+    /**
+     * Determine if the validation rule passes.
+     *
+     * @param  string  $attribute
+     * @param  mixed  $value
+     * @return bool
+     */
+    public function passes($attribute, $value)
+    {
+  
+      $explodeAttributes = explode(".",$attribute);
+
+      $items = \DB::connection(env('DB_CONNECTION', 'mysql'))->table($this->table)
+        ->where($explodeAttributes[0],$value);
+      
+      if($this->id){
+        $items = $items->where($this->columnId, "!=", $this->id);
+      }
+
+      $items = $items->first();
+
+      return !$items;
+    }
+
+    /**
+     * Get the validation error message.
+     *
+     * @return string
+     */
+    public function message()
+    {
+        return $this->message;
+    }
+}
