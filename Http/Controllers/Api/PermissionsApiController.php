511025539861d35d6e98ef463ef1aad1c8acb7e9
Juan Escalante <jcec007@gmail.com>
2022-02-08 16:37:04 -0500

    +Update: PermissionsApiController::28 | changed Role query by a call to the RoleRepository because the repository has the validation when the Tenant must be with the central data too.

--- Http/Controllers/Api/PermissionsApiController.php
+++ Http/Controllers/Api/PermissionsApiController.php
@@ -9,10 +9,11 @@ use Illuminate\Support\Arr;
 
 class PermissionsApiController extends BasePublicController
 {
+  
   //Get settings by relatedId and entityName
   public function index($params = [])
   {
-
+    
     $params = (object)$params;//Conver params
     if (!$params || !$params->relatedId || !$params->entityName) return [];//Validate params
     $permissions = [];//Default response
@@ -25,8 +26,10 @@ class PermissionsApiController extends BasePublicController
       $permissionsData = User::whereIn('id', $params->relatedId)->get()->pluck('permissions')->toArray();
 
     //Get role permissons
-    if ($params->entityName == 'role')
-      $permissionsData = Role::whereIn('id', $params->relatedId)->get()->pluck('permissions')->toArray();
+    if ($params->entityName == 'role'){
+      $roleRepository = app("Modules\Iprofile\Repositories\RoleApiRepository");
+      $permissionsData = $roleRepository->getItemsBy(json_decode(json_encode(["filter" => ["id" => $params->relatedId]])))->pluck('permissions')->toArray();
+    }
 
 
     //Merge all permissions
@@ -52,12 +55,13 @@ class PermissionsApiController extends BasePublicController
     if (!isset($params->userId) || !$params->userId) return [];//Validate userID params
     
     $user = User::with('roles')->where("id",$params->userId)->first();//Get user data
+
     if (!isset($params->roleId) || !$params->roleId) $params->roleId = $user->roles->pluck('id')->toArray();//Validate roleId
-    
+
     //Get settings per entity
     $userPermissions = $this->index(['relatedId' => $params->userId, 'entityName' => 'user']);
     $rolePermissions = $this->index(['relatedId' => $params->roleId, 'entityName' => 'role']);
-    
+
     //Merge all settings with priority
     $permissions = array_merge($rolePermissions, $userPermissions);
 
